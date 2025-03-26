c5f4c6566d6ed8ed6d1a7e937bf413449b2b8a5b
SebastianPerilla <sebastianperillaespinosa@gmail.com>
2025-03-26 19:06:24 +0100

    Trying to add in the new Macro Sentiment Model

--- Home.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.title('Home')
-st.write('Welcome to the home page of the EDEN Fund Sentiment Analysis Dashboard.')
-
-st.text('There are two applications for the model, one being that of the Macro Team and the other of the Sentiment Team')
